b75db576ad9020027876ab595e417a326f88472f
mc8na <36861633+mc8na@users.noreply.github.com>
2022-02-28 08:52:33 -0500

    Added files to client folder

---
+++ client/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer>
+      <p className="text-center">
+  <a>Portfolio: Miles Clikeman - 2022</a>
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
